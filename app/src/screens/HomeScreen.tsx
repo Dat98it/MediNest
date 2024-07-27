239e9a5d0322b88384c6a30cdff75ad931af9877
dat98it <dat98.it@gmail.com>
2024-07-27 08:56:26 +0700

    [app] config react native vector icons

--- app/src/screens/HomeScreen.tsx
+++ app/src/screens/HomeScreen.tsx
@@ -5,6 +5,7 @@ import {RootStackParamList} from '@src/types';
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {Button} from 'react-native-paper';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 type HomeScreenNavigationProp = NativeStackNavigationProp<
   RootStackParamList,
@@ -16,6 +17,7 @@ export const HomeScreen = () => {
 
   return (
     <View>
+      <Icon name="comments" size={30} color="green" />
       <MyIcon />
       <Text style={styles.title}>Home Screen</Text>
       <Button
